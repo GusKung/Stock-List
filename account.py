2c035d47f72e3bf447788ae3ccac0549e9463cd1
GusKung <aumnaet7540@gmail.com>
2025-09-30 22:29:46 +0700

    File

--- account.py
+++ account.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv, dotenv_values
 from CTkScrollableDropdown import *
 from customtkinter import *
 import customtkinter
+from tkinter import ttk
 
 def gui_account(my_sql):
 # //-----------------------------------------------------
@@ -33,207 +34,140 @@ def gui_account(my_sql):
             
             self.iconbitmap(icon)
             self.after(200, lambda: self.iconbitmap(icon)) 
-            self.resizable(False, False)
 
 # //-----------------------------------------------------
 
-    account_gui = CTKUI("Stock List", 650, 400, "#D4D4D4", "light")
+    account_gui = CTKUI("Stock List", 1280, 600, "#7cffac", "light")
     account_gui.attributes("-topmost",True)
 
-    Main_Frame = CTkFrame(account_gui, fg_color="#D4D4D4",border_width=0)
-    Main_Frame.pack(fill=BOTH,expand=True)
+    FLeft = CTkFrame(account_gui,width=400,fg_color="#7cffac", corner_radius=0)
+    FLeft.pack(side=LEFT, fill=BOTH)
 
-    scroll_gui = CTkScrollableFrame(Main_Frame,fg_color="#D4D4D4",border_width=0)
-    scroll_gui.pack(fill=BOTH,expand=True)
+    FLeft.columnconfigure(0,weight=1)
+    FLeft.rowconfigure(2,weight=1)
+
+    FRight = CTkFrame(account_gui,fg_color="#FFFFFF", corner_radius=0)
+    FRight.pack(side=RIGHT, fill=BOTH,expand=True)
+
+    ID = StringVar(value="")
+    USER = StringVar(value="")
+    PASS = StringVar(value="")
+    LEVEL = StringVar(value="")
+
+    inp_user = CTkEntry(FLeft,placeholder_text="Username",text_color="#818181",textvariable=USER,width=200,corner_radius=20,height=30,border_width=2,border_color="#8D8D8D",fg_color="#FCFCFC",font=("Arial", 16),state="disabled")
+    inp_user.grid(row=0,column=0,pady=(40,10),padx=20)
+
+    inp_level = CTkComboBox(FLeft,values=["1","2","3"],variable=LEVEL,state="disabled",width=100,corner_radius=20,height=30,border_width=2,border_color="#8D8D8D",fg_color="#FCFCFC",font=("Arial", 16))
+    inp_level.grid(row=0,column=1,pady=(40,10),padx=20)
+
+    inp_pass = CTkEntry(FLeft,placeholder_text="Password",text_color="#818181",textvariable=PASS,show="●",width=200,corner_radius=20,height=30,border_width=2,border_color="#8D8D8D",fg_color="#FCFCFC",font=("Arial", 16),state="disabled")
+    inp_pass.grid(row=1,column=0,pady=20,padx=20)
+
+    def edit():
+        if (inp_user.cget("state") == "disabled"):
+            inp_user.configure(state="normal",text_color="#000000",border_color="#000000",fg_color="#FFFFFF")
+            inp_level.configure(state="normal",border_color="#000000",fg_color="#FFFFFF")
+            inp_pass.configure(state="normal",text_color="#000000",border_color="#000000",fg_color="#FFFFFF")
+            data_employee.configure(state="normal",text_color="#000000",border_color="#8D8D8D",fg_color="#FFFFFF")
+            btn_edit.configure(text="Save")
+        else:
+            inp_user.configure(state="disabled",text_color="#818181",border_color="#8D8D8D",fg_color="#FCFCFC")
+            inp_level.configure(state="disabled",border_color="#8D8D8D",fg_color="#FCFCFC")
+            inp_pass.configure(state="disabled",text_color="#818181",border_color="#8D8D8D",fg_color="#FCFCFC")
+            data_employee.configure(state="disabled",text_color="#818181",border_color="#A7A7A7",fg_color="#FCFCFC")
+            btn_edit.configure(text="Edit")
+
+            print(ID.get(),USER.get(),LEVEL.get(),PASS.get())
+            print(data_employee.get("1.0","1.end"))
+            print(data_employee.get("2.0","2.end"))
+            print(data_employee.get("3.0","3.end"))
+            print(data_employee.get("4.0","4.end"))
+
+
+    btn_edit = CTkButton(FLeft,text="Edit",width=150,corner_radius=14,height=30,font=("Arial", 16),command=edit)
+    btn_edit.grid(row=1,column=1,pady=20,padx=20)
+
+    data_employee = CTkTextbox(FLeft,width=400,height=200,corner_radius=20,font=("Arial", 16),border_width=2,border_color="#A7A7A7",fg_color="#FCFCFC",text_color="#818181")
+    data_employee.configure(state="disabled")
+    data_employee.grid(row=2,column=0,columnspan=2,pady=10,padx=20,sticky="nsew")
+
+    style = ttk.Style(account_gui)
+    style.configure("Treeview", font=("Arial", 12), rowheight=40,borderwidth=0, highlightthickness=0)       
+    style.configure("Treeview.Heading", font=("Arial Bold", 12))  
+
+    table_account = ttk.Treeview(FRight,columns=("ID_EM","ID","Username","Level","Employee","First_Name","Last_Name","Password","Address","Contact"),show="headings")
+
+    scrollbar = ttk.Scrollbar(FRight, orient="vertical", command=table_account.yview)
+    table_account.configure(yscrollcommand=scrollbar.set)
+    scrollbar.pack(side="right", fill=Y)
+
+    table_account.heading("ID",text="ID")
+    table_account.heading("Username",text="Username")
+    table_account.heading("Level",text="Level")
+    table_account.heading("Employee",text="Employee")
+
+    table_account.column("ID_EM",width=0,stretch=False)
+    table_account.column("ID",width=30,anchor=CENTER)
+    table_account.column("Username",width=100,anchor=CENTER)
+    table_account.column("Level",width=30,anchor=CENTER)
+    table_account.column("Employee",width=100,anchor=CENTER)
+
+    table_account.column("First_Name",width=0,stretch=False)
+    table_account.column("Last_Name",width=0,stretch=False)
+    table_account.column("Password",width=0,stretch=False)
+    table_account.column("Address",width=0,stretch=False)
+    table_account.column("Contact",width=0,stretch=False)
+
+    table_account.pack(fill=BOTH,expand=True,pady=20,padx=20)
+
+    def select_data(event):
+        select = table_account.focus()
+        values = table_account.item(select,"values")
+
+        id_employee = values[0]
+        id_user = values[1]
+        user = values[2]
+        password = values[7]
+        level = values[3]
+
+        employee = values[4]
+
+        f_name = values[5]
+        l_name = values[6]
+        
+        address = values[8]
+        contact = values[9]
+
+        ID.set(id_user)
+        USER.set(user)
+        PASS.set(password)
+        LEVEL.set(level)
+
+        data_employee.configure(state="normal")
+        data_employee.delete("0.0","end")
+        data_employee.insert("1.0",f"Employee ID : {id_employee}\n\n")
+        data_employee.insert("2.0",f"Name : {f_name} {l_name}\n\n")
+        data_employee.insert("3.0",f"Address : {address}\n\n")
+        data_employee.insert("4.0",f"Contact : {contact}\n\n")
+        data_employee.configure(state="disabled")
+
+    table_account.bind("<Double-1>", select_data)
 
     sql = my_sql.cursor()
-    sql.execute("SELECT * FROM stock_list.accounts order by `level` desc;")
+    sql.execute("SELECT * FROM stock_list.employee join `stock_list`.`accounts` on `employee`.`account_id` = `accounts`.`id` order by `accounts`.`level` desc;")
     resulte = sql.fetchall()
-# //-----------------------------------------------------------------------------------------
 
-    data_list = []
-    
-    count = -1
-# //-----------------------------------------------------------------------------------------
-    
     for i in resulte:
-        data_user = {}
-        id = i[0]
-        data_user["id"] = id
-        username = StringVar(scroll_gui,value=i[1])
-        password = StringVar(scroll_gui,value=i[2])
-        level = StringVar(scroll_gui,value=i[3])
+        id_employee = i[0] 
+        id_user = i[1]
+        user = i[7]
+        level = i[9]
+        employee = i[2]
 
-        Frame = CTkFrame(scroll_gui, fg_color="#FFFFFF")
-        Frame.pack(padx=20, pady=5,fill=X)
+        f_name = i[2]
+        l_name = i[3]
+        password = i[8]
+        address = i[4]
+        contact = i[5]
 
-        Username_Label = CTkLabel(Frame,text="Username:",font=("Arial",16),text_color="#000000",bg_color="#FFFFFF")
-        Username_Label.grid(row=0,column=0,padx=(20,10),pady=20,sticky="w")
-
-        data_user["inp_user"] = CTkEntry(Frame,placeholder_text="Username",textvariable=username,state="disabled",border_color="#C4C4C4",border_width=2,width=150,height=30,corner_radius=20,fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-        data_user["inp_user"].grid(row=0,column=1,padx=(20,10),pady=20,sticky="w")
-
-        data_user["btn_level"] = CTkComboBox(Frame,width=100,height=30,corner_radius=20,variable=level,fg_color="#FFFFFF",font=("Arial",16),text_color="#000000",values=["1","2","3"],state="disabled")
-        data_user["btn_level"].grid(row=0,column=2,padx=(20,10),pady=20,sticky="e")
-
-        Password_Label = CTkLabel(Frame,text="Password:",font=("Arial",16),text_color="#000000",bg_color="#FFFFFF")
-        Password_Label.grid(row=1,column=0,padx=(20,10),pady=20,sticky="w")
-
-        data_user["inp_password"] = CTkEntry(Frame,placeholder_text="Password",textvariable=password,show="●",state="disabled",border_color="#C4C4C4",border_width=2,width=200,height=30,corner_radius=20,fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-        data_user["inp_password"].grid(row=1,column=1,padx=(20,10),pady=20,sticky="w")
-
-        data_list.append(data_user)
-
-        def edit(user,pas,lv,btn,num):
-            
-            if (user.cget("state") == "disabled"):
-                user.configure(state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000")
-
-                pas.configure(state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000")
-
-                lv.configure(state="normal",fg_color="#FFFFFF",font=("Arial",16),text_color="#000000")
-
-                btn.configure(text="Save")
-            else:
-                user.configure(state="disabled",border_color="#C4C4C4",fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-
-                pas.configure(state="disabled",border_color="#C4C4C4",fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-
-                lv.configure(state="disabled",fg_color="#FFFFFF",font=("Arial",16),text_color="#000000")
-
-                # print( num, user.get(), pas.get(), lv.get())
-
-                data_list[num]["inp_user"].delete(0, 'end')
-                data_list[num]["inp_user"].insert(0,user.get())
-
-                data_list[num]["inp_password"].delete(0, 'end')
-                data_list[num]["inp_password"].insert(0,pas.get())
-
-                data_list[num]["btn_level"].set(lv.get())
-
-                # print( data_list[num]["id"],data_list[num]["inp_user"].get(), data_list[num]["inp_password"].get() , data_list[num]["btn_level"].get())
-
-                btn.configure(text="Edit")
-                
-        count += 1
-
-        data_user["btn_edit"] = CTkButton(Frame,text="Edit",font=("Arial",16),text_color="#FFFFFF",fg_color="#4B8BBE",hover_color="#306998",width=100,height=40,corner_radius=20)
-        data_user["btn_edit"].configure(command=lambda e_user = data_user["inp_user"], e_pas = data_user["inp_password"], e_lv = data_user["btn_level"] , e_btn= data_user["btn_edit"] , e_num=count:edit(e_user,e_pas,e_lv,e_btn,e_num))
-        data_user["btn_edit"].grid(row=1,column=2,padx=(40,10),pady=20,sticky="e")
-
-        btn_remove = CTkButton(Frame,width=40,height=30,corner_radius=20,text="Remove",text_color="white",fg_color="#f33838",hover_color="#f66b6b")
-        btn_remove.grid(row=1,column=3,padx=(0,20),pady=(0,80),sticky="nse")
-
-# //----------------------------------------------------------------------------------------
-    re_count = 0
-    new_count = count
-    def add_tarck():
-# //-----------------------------------------------------------------------------------------
-        new_data_list = []
-        nonlocal re_count, new_count
-# //-----------------------------------------------------------------------------------------
-        new_data_user = {}
-        username = StringVar(scroll_gui,value="")
-        password = StringVar(scroll_gui,value="")
-        level = StringVar(scroll_gui,value="")
-
-        Frame = CTkFrame(scroll_gui, fg_color="#FFFFFF")
-        Frame.pack(padx=20, pady=5,fill=X)
-
-        Username_Label = CTkLabel(Frame,text="Username:",font=("Arial",16),text_color="#000000",bg_color="#FFFFFF")
-        Username_Label.grid(row=0,column=0,padx=(20,10),pady=20,sticky="w")
-
-        new_data_user["inp_user"] = CTkEntry(Frame,placeholder_text="Username",textvariable=username,state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000",corner_radius=20)
-        new_data_user["inp_user"].grid(row=0,column=1,padx=(20,10),pady=20,sticky="w")
-
-        new_data_user["btn_level"] = CTkComboBox(Frame,width=100,height=30,corner_radius=20,variable=level,values=["1","2","3"],state="normal",fg_color="#FFFFFF",font=("Arial",16),text_color="#000000")
-        new_data_user["btn_level"].grid(row=0,column=2,padx=(20,10),pady=20,sticky="e")
-
-        Password_Label = CTkLabel(Frame,text="Password:",font=("Arial",16),text_color="#000000",bg_color="#FFFFFF")
-        Password_Label.grid(row=1,column=0,padx=(20,10),pady=20,sticky="w")
-
-        new_data_user["inp_password"] = CTkEntry(Frame,placeholder_text="Password",textvariable=password,show="●",state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000",border_width=2,width=200,height=30,corner_radius=20)
-        new_data_user["inp_password"].grid(row=1,column=1,padx=(20,10),pady=20,sticky="w")
-
-        new_data_list.append(new_data_user)
-
-        def edit(user,pas,lv,btn,num,new_num):
-            
-            if (user.cget("state") == "disabled"):
-                user.configure(state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000")
-
-                pas.configure(state="normal",border_color="#000000",fg_color="#FCFCFC",font=("Arial",16),text_color="#000000")
-
-                lv.configure(state="normal",fg_color="#FFFFFF",font=("Arial",16),text_color="#000000")
-
-                btn.configure(text="Save")
-            else:
-                user.configure(state="disabled",border_color="#C4C4C4",fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-
-                pas.configure(state="disabled",border_color="#C4C4C4",fg_color="#FCFCFC",font=("Arial",16),text_color="#3F3F3F")
-
-                lv.configure(state="disabled",fg_color="#FFFFFF",font=("Arial",16),text_color="#000000")
-
-                # new_data_list[num]["inp_user"].delete(0, 'end')
-                # new_data_list[num]["inp_user"].insert(0,user.get())
-
-                # new_data_list[num]["inp_password"].delete(0, 'end')
-                # new_data_list[num]["inp_password"].insert(0,pas.get())
-
-                # new_data_list[num]["btn_level"].set(lv.get())
-
-                data_list.append(new_data_list[num])
-                print(data_list[new_num]["inp_user"].get(), data_list[new_num]["inp_password"].get() , data_list[new_num]["btn_level"].get() )
-
-                btn.configure(text="Edit")
-                
-        new_count += 1
-
-        print(new_count)
-        print(re_count)
-
-        new_data_user["btn_edit"] = CTkButton(Frame,text="Save",font=("Arial",16),text_color="#FFFFFF",fg_color="#4B8BBE",hover_color="#306998",width=100,height=40,corner_radius=20)
-        new_data_user["btn_edit"].configure(command=lambda e_user = new_data_user["inp_user"], e_pas = new_data_user["inp_password"], e_lv = new_data_user["btn_level"] , e_btn= new_data_user["btn_edit"] , e_num=re_count,e_newnum = new_count:edit(e_user,e_pas,e_lv,e_btn,e_num,e_newnum))
-        new_data_user["btn_edit"].grid(row=1,column=2,padx=(40,10),pady=20,sticky="e")
-
-        btn_remove = CTkButton(Frame,width=40,height=30,corner_radius=20,text="Remove",text_color="white",fg_color="#f33838",hover_color="#f66b6b")
-        btn_remove.grid(row=1,column=3,padx=(0,20),pady=(0,80),sticky="nse")
-
-    # //----------------------------------------------------------------------------------------
-
-    btn_add = CTkButton(account_gui,command=add_tarck,text="Add",font=("Arial",16),text_color="#FFFFFF",fg_color="#38f388",hover_color="#6be59e",width=100,height=40,corner_radius=20,bg_color="#D4D4D4")
-    btn_add.pack(pady=(0,10),padx=(20,20),side=LEFT)
-
-    def apply():
-        for i in data_list:
-            try:
-                id = int(i["id"])
-                user = str(i["inp_user"].get())
-                pas = str(i["inp_password"].get())
-                lv = int(i["btn_level"].get())
-
-                print(id,user,pas,lv)
-
-                sql = my_sql.cursor()
-                sql.execute("SELECT * FROM stock_list.accounts WHERE `id` = '%d' AND `username` = '%s' AND `passwords` = '%s' AND `level` = '%d' ;" % (id,user,pas,lv))
-                resulte = sql.fetchall()
-                if (resulte):
-                    pass
-                else:
-                    sql.execute("UPDATE stock_list.accounts SET `username` = '%s' , `passwords` = '%s' , `level` = '%d' WHERE `id` = '%d' ;" % (user,pas,lv,id))
-                    my_sql.commit()
-                    Message = CTkMessagebox(title="Success",message="Update Success",icon="check",option_1="OK")
-                    if (Message.get() == "OK"):
-                        account_gui.destroy()
-            except KeyError as e:
-                id = ""
-                user = str(i["inp_user"].get())
-                pas = str(i["inp_password"].get())
-                lv = int(i["btn_level"].get())
-
-                print(id,user,pas,lv)
-
-    btn_apply= CTkButton(account_gui,text="Apply",font=("Arial",16),text_color="#FFFFFF",fg_color="#38f388",hover_color="#6be59e",width=100,height=40,corner_radius=20,bg_color="#D4D4D4",command=apply)
-    btn_apply.pack(pady=(0,10),padx=(20,20),side=RIGHT)
-
-    
+        table_account.insert("",END,values=(id_employee,id_user,user,level,employee,f_name,l_name,password,address,contact))
