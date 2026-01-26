c505cc82677d0bfc60126acd276b53b214401720
GusKung <aumnaet7540@gmail.com>
2026-01-26 23:28:11 +0700

    File

---
+++ encode_file.py
@@ -0,0 +1,111 @@
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv, dotenv_values 
+import io
+import os
+from pathlib import Path
+
+class EncodeDecode:
+    def __init__(self):
+        self.appdir = Path(__file__).parent
+        self.doc = self.check_doc()
+        self.stocklist_dir = self.doc / "Stock List"
+        self.account_file = self.stocklist_dir / "account.env"
+        self.server_file = self.stocklist_dir / "server.env"
+        self.key = self.appdir / "mykey.key"
+
+        self.server = [
+            "HOST=localhost",
+            "USER=root",
+            "PASSWORD=",
+            "DATABASE=stock_list",
+            "TIME_ZONE=+07:00"
+        ]
+        self.account = [
+            "USERNAME=admin",
+            "PASSWORD=1234",
+            "REMEMBER=False",
+            "POS=1"
+        ]
+
+    def load_data(self):
+        data_server = {}
+        data_account = {}
+
+        if (not self.stocklist_dir.exists()):
+            os.makedirs(self.stocklist_dir)
+
+        if (not self.server_file.exists()):
+            with open(self.server_file, "wb") as ds:
+                ds.write("\n".join(self.server).encode("utf-8"))
+            self.encode(self.server_file)
+            self.decode(self.server_file)
+        else:
+            server = self.decode(self.server_file)
+            data_server.update(server)
+            
+        
+        if (not self.account_file.exists()):
+            with open(self.account_file, "wb") as da:
+                da.write("\n".join(self.account).encode("utf-8"))
+            self.encode(self.account_file)
+            self.decode(self.account_file)
+        else:
+            account = self.decode(self.account_file)
+            data_account.update(account)
+
+        return {**data_server, **data_account,"Server_File":self.server_file,"Account_File":self.account_file}
+           
+    
+    def encode(self, filepath):
+        fernet = Fernet(open(self.key, "rb").read())
+
+        with open(filepath, "rb") as file:
+            original = file.read()
+
+        encrypted = fernet.encrypt(original)
+
+        with open(filepath, "wb") as encrypted_file:
+            encrypted_file.write(encrypted)
+
+    def decode(self, filepath):
+        fernet =Fernet(open(self.key, "rb").read())
+
+        with open (filepath, "rb") as enc_file:
+            encrypted = enc_file.read()
+
+        decrypted = fernet.decrypt(encrypted).decode("utf-8")
+
+        data = io.StringIO(decrypted)
+
+        config = dotenv_values(stream=data)
+
+        return config
+
+    def edit_data(self, filepath, new_data):
+        fernet =Fernet(open(self.key, "rb").read())
+
+        with open (filepath, "rb") as enc_file:
+            encrypted = enc_file.read()
+
+        decrypted = fernet.decrypt(encrypted).decode("utf-8")
+
+        data = io.StringIO(decrypted)
+
+        config = dotenv_values(stream=data)
+        config.update({item.split("=")[0]: item.split("=")[1] for item in new_data})
+        new_content_lines = [f"{key}={value}" for key, value in config.items()]
+
+        with open(filepath, "w") as file:
+            file.write("\n".join(new_content_lines))
+        
+        self.encode(filepath)
+        
+    def check_doc(self):
+        user_home = Path.home()
+        self.doc_folder = user_home / "Documents"
+
+        if (not self.doc_folder.exists()):
+            
+            self.doc_folder = user_home / "OneDrive" / "Documents"
+
+        return self.doc_folder
--- login.py
+++ login.py
@@ -3,29 +3,117 @@ import customtkinter
 from pathlib import Path
 from CTkMessagebox import *
 from CTkMenuBar import *
+from PIL import Image
+import encode_file
 
 class LoginApp(CTk):
-    def __init__(self,titel,wide,height,color,theme):
-        CTk.__init__(self)
-        self.title(f"{titel}")
+    def __init__(self,title,width,height,color,theme):
+        super().__init__()
+        self.title(f"{title}")
         screen_x = self.winfo_screenwidth()
         screen_y = self.winfo_screenheight()
-
-        x = int((screen_x / 2) - (wide/2))
-        y = int((screen_y / 2) - (height / 2))
-        self.geometry(f"{wide}x{height}+{x}+{y-25}")
-        
-        self.config(bg=f"{color}")
         customtkinter.set_appearance_mode(theme)
 
-        appdir = Path(__file__).parent
-        icon = appdir / "icon" / "icon.ico"
-        self.iconbitmap(icon)
+        x = int((screen_x / 2) - (width/2))
+        y = int((screen_y / 2) - (height / 2))
+        self.geometry(f"{width}x{height}+{x}+{y-25}")
+
+        self.configure(fg_color=f"{color}")
+
+        self.appdir = Path(__file__).parent
+        self.icon = self.appdir / "icon" / "icon.ico"
+        self.iconbitmap(self.icon)
+        
+        self.login_ui()
+        self.load_data = encode_file.EncodeDecode().load_data()
+        self.data = encode_file.EncodeDecode()
+
+        self.host = self.load_data.get("HOST")
+        self.user = self.load_data.get("USER")
+        self.pwd = self.load_data.get("PASSWORD")
+        self.database = self.load_data.get("DATABASE")
+        self.time_zone = self.load_data.get("TIME_ZONE")
+
+        self.username = self.load_data.get("USERNAME")
+        self.passwords = self.load_data.get("PASSWORD")
+        self.remember_me = self.load_data.get("REMEMBER")
+        self.pos = self.load_data.get("POS")
+
+        self.file_server = self.load_data.get("Server_File")
+        self.file_account = self.load_data.get("Account_File")
+
+        self.account_data = self.data.edit_data(self.file_account,[
+            "USERNAME=admin",
+            "PASSWORD=1234",
+            "REMEMBER=False",
+            "POS=1"
+        ])
+
+    def login_ui(self):
+        self.user = StringVar(value="")
+        self.pwd = StringVar(value="")
+        self.remember_var = BooleanVar(value=False)
+
         self.menu = CTkTitleMenu(self)
         self.file_menu = self.menu.add_cascade("File")
-        def about():
-            CTkMessagebox(title="About", message="\tProgram : Stock List\n\n\tVersion : 1.0\n\n\tDevelop By August_Tas", icon="info", option_1="OK")
-        self.file_about = self.menu.add_cascade("About",command=about)
+        self.file_about = self.menu.add_cascade("About",command=self.about)
+        
+        self.frameleft = CTkFrame(self, width=400, height=400, fg_color="#3afa80")
+        self.frameleft.pack(side=LEFT, fill=BOTH, expand=YES)
+
+        self.img_icon = self.appdir / "icon" / "icon.png"
+        self.open_img = Image.open(self.img_icon)
+
+        self.ctk_img_icon = CTkImage(self.open_img, size=(160,160))
+
+        self.label_icon = CTkLabel(self.frameleft, image=self.ctk_img_icon, text="")
+        self.label_icon.pack(fill=Y, expand=YES)
+
+        self.frameright = CTkFrame(self, width=340, height=400, fg_color="#FFFFFF")
+        self.frameright.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+        self.label_title = CTkLabel(self.frameright, text="Stock List", font=("Bold Arial",45))
+        self.label_title.place(relx=0.15, rely=0.1, anchor="nw")
+
+        self.inp_user = CTkEntry(self.frameright, width=300, height=40, placeholder_text="Username",fg_color="transparent",border_width=0,font=("Arial",16))
+        self.inp_user.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+        self.inp_pwd = CTkEntry(self.frameright, width=300, height=40, placeholder_text="Password", show="●",fg_color="transparent",border_width=0,font=("Arial",16))
+        self.inp_pwd.place(relx=0.5, rely=0.45, anchor=CENTER)
+
+        self.remember = CTkCheckBox(self.frameright, text="Remember")
+        self.remember.place(relx=0.15, rely=0.6, anchor="nw")
+
+        self.btn_login = CTkButton(self.frameright, width=200, height=40, text="Login")
+        self.btn_login.place(relx=0.9, rely=0.59, anchor="ne")
+
+        self.btn_login.bind("<Enter>", lambda event: self.hover_enter())
+        self.btn_login.bind("<Leave>", lambda event: self.hover_leave())
+
+        self.update()
+
+        self.border_bottom(self.inp_user,self.frameright)
+        self.border_bottom(self.inp_pwd,self.frameright)
+
+        self.inp_pwd.focus()
+
+        
+
+    def about(self):
+        CTkMessagebox(title="About", message="\tProgram : Stock List\n\n\tVersion : 1.0\n\n\tDevelop By August_Tas", icon="info", option_1="OK")
+
+    def border_bottom(self,obj1,obj2):
+        p_x = obj1.winfo_x()
+        p_y = obj1.winfo_y() + obj1.winfo_height()
+
+        boder = CTkCanvas(obj2,width=300,height=2,bg="black")
+        boder.place(x=p_x,y=p_y,anchor="sw",relwidth=.75)
+
+    def hover_enter(self):
+        self.config(cursor="hand2")
+
+    def hover_leave(self):
+        self.config(cursor="arrow")
 
 if __name__ == "__main__":
     Login = LoginApp("Login",640,480,"#FFFFFF","light")
